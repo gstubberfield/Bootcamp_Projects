0db840067cd73c80ec9cb1a1ed29a9f17ebef876
Gemma Stubberfield <gstubberfield@gmail.com>
2024-11-18 09:26:33 +0000

    Cafe.py

--- cafe.py
+++ cafe.py
@@ -1,9 +1,8 @@
-# Create a list called menu and add items sold in a cafe
+# Create a list to add items sold in a cafe.
 
 menu = ["Coffee", "Cake", "Ice Cream", "Hot Chocolate"]
 
-# Create a dictionary called stock
-# Add the stock value for each item on menu
+# Create a dictionary to add the stock value for each item on menu.
 
 stock = {
     "Coffee" : 250,
@@ -12,8 +11,7 @@ stock = {
     "Hot Chocolate" : 120
 }
 
-# Create dictionary called price
-# Add prices of each item on menu
+# Create dictionary to add prices of each item on menu.
 
 price = {
     "Coffee" : 3.20,
@@ -22,9 +20,7 @@ price = {
     "Hot Chocolate" : 4.60
 }
 
-# Calculate the worth of total_stock in cafe
-# Loop through dictionaries and lists
-# Set list items as keys to access corresponding stock and price
+# Calculate the worth of total_stock in cafe, looping through dictionaries and lists.
 
 total_stock = 0
 for value in menu:
